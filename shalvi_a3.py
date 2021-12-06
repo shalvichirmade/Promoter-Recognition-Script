4be0c1532de3068d8a71da7c01b01141fe0ea570
Shalvi Chirmade <92757717+shalvichirmade@users.noreply.github.com>
2021-12-06 10:16:30 -0500

    Working update.

--- shalvi_a3.py
+++ shalvi_a3.py
@@ -40,7 +40,7 @@ for line in fg_file.readlines():
 fg_file.close()
 
 #Open each FASTA file and store its DNA sequence.
-complete_dna = [] #A list where every element is the DNA sequence from each FASTA file.
+complete_dna = [] #A list where every element is the DNA sequence from each FASTA file
 num = 1
 print("This step takes a long time, please be patient.")
 
@@ -58,7 +58,7 @@ for name in fasta_file_names:
     fasta.close()
 
 #Open each GFF3 file and store lines for all genes.
-complete_gff3 = [] #A list where every element is the gff3 file containing gene lines. Each element is a list, where each element is each gene line.
+complete_gff3 = [] #A list where every element is the gff3 file containing gene lines. Each element is a list, where each element is each gene line
 num = 1
 print("Inputting GFF3 files.")
 
@@ -77,5 +77,60 @@ for name in gff3_file_names:
     num += 1
     gff3.close()
 
+#Read in the gene names and motif sequence files.
+gene_name_file = open(file_path + "/zea_mays_genes.txt")
+genes_list = [] #Make a list of the co-expressed genes
+for line in gene_name_file:
+    genes_list.append(line.rstrip())
+
+gene_name_file.close()
+
+motif_file = open(file_path + "/promoters.txt")
+motif_list = [] #Make a list of the motif sequences
+for line in motif_file:
+    motif_list.append(line.rstrip())
+
+motif_file.close()
+
+
+#Create a GFF object to extract the features we require for further analysis. 
+class GFF(object):    
+    def __init__(self, data):  
+        line_list = data.split("\t")
+        self.chr = int(line_list[0]) #chromosome number
+        self.start = int(line_list[3])
+        self.end = int(line_list[4])
+        self.strand = str(line_list[6])
+        long_name = line_list[8].split(";")[0] #the first part of the name
+        self.name =  long_name.split(":")[1] #extracting the exact gene name as written in zea_mays_genes.txt
+
+#Use GFF class to extract this information for every gene in complete_gff list.
+complete_class_gff3 = [] #Create a list for every gene in all the gff3 files
+
+for file in complete_gff3: #Remember this was a list of lists
+    for gene in file:
+        complete_class_gff3.append(GFF(gene))
+
+
+#Use the names of the co-expressed genes to create a list only containing the GFF3 lines for those genes.
+selected_genes_class_gff3 = [] #Make a list of only the genes present in the co-expressed gene list
+#Be aware that the gene order in this list is not the same as the gene order in genes_list.
+
+for value in complete_class_gff3:
+    if (value.name in genes_list):
+        selected_genes_class_gff3.append(value)
+
+#Check to see if there any duplicated genes in your list.
+if len(genes_list) == len(selected_genes_class_gff3):
+    print("\nThere are no multiple TSSs for your genes.")
+elif len(genes_list) > len(selected_genes_class_gff3):
+    print("\nThere are some genes from your list that have not been found. Please make sure you have included the correct gene names and restart this program.")
+else:
+    print("\nYou may have multiple TSSs for a few genes. Please choose the most upstream one.")
+
+
+
+
+    
 
 
